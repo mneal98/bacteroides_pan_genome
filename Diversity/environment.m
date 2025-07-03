fd7ce05410522aaa73360c847863683ce317d9b5
mneal98 <mneal@ucsd.edu>
2025-07-03 11:55:57 -0700

    Delete Diversity directory

--- Diversity/environment.m
+++
@@ -1,7 +0,0 @@
-%function that gives active exchanges and their bounds.
-function [env]=environment(model)
-    ex_ind=find(startsWith(model.rxns,'EX_'));
-    active_ind=find(model.lb<0);
-    active_ex_ind=intersect(ex_ind,active_ind);
-    env=[model.rxns(active_ex_ind),num2cell(model.lb(active_ex_ind))];
-end
