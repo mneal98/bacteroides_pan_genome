6ec262eefb35a0503b0f101bf1d563ccbfe2f735
mneal98 <mneal@ucsd.edu>
2025-07-03 11:56:14 -0700

    Delete No IBD correlation directory

--- No IBD correlation/chi2ind.m
+++
@@ -1,8 +0,0 @@
-function [h, chi, p] = chi2ind(x, alpha)
-    expected = (sum(x,2)*sum(x))/sum(sum(x));
-    OE = ((x - expected).^2)./expected;
-    chi = sum(sum(OE,2));
-    df = (size(x, 1)-1)*(size(x,2)-1);
-    p = 1 - chi2cdf(chi,df);
-    h = p>=alpha;
-end
